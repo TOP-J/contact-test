c01c3f8b89112698fe4a2a09d2582789c4082ab8
baba~J <nkehhotter@gmail.com>
2022-07-08 22:19:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const ejs = require('ejs');
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+var array = ['add new items with +'];
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.get('/', function(req, res){
+  res.render('contact', {array:array});
+});
+app.post('/', function(req,res){
+const newItem = req.body.newItem;
+array.push(newItem);
+res.redirect('/');
+});
+
+
+
+
+app.listen(PORT, function(){
+  console.log("listening on port $'PORT'");
+});
